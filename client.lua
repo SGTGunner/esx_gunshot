9158465c8dd524474d6950f1eb697fe4a28ffe8a
Henric Johansson <henric-johansson@hotmail.com>
2018-04-22 00:48:18 +0200

    Fixade så man kan kolla om alla spelare har krutstänk, optimerade lite, lade till en timer på krutstänkets livslängd, hämtade playerped,

--- client.lua
+++ client.lua
@@ -1,17 +1,7 @@
-ESX               = nil
-local hasShot 	  = false
-
-Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
-end)
-
-RegisterNetEvent('esx:playerLoaded')
-AddEventHandler('esx:playerLoaded', function(xPlayer)
-  PlayerData = xPlayer
-end)
+ESX               					= nil
+local hasShot 	  					= false
+local playerPed 					= nil
+local GunpowderSaveTime 			= 15 * 60 * 1000 -- krutstänk sparas 15 minuter standard
 
 ----- Kommando
 
@@ -19,59 +9,69 @@ RegisterCommand("guntest", function(source)
     TriggerEvent("esx_guntest:checkGun", source)
 end, false)
 
------ Kod
 
-RegisterNetEvent('esx_guntest:hasShotGun')
-AddEventHandler('esx_guntest:hasShotGun', function()
-	gunIsShot = true
-end)
-
-RegisterNetEvent('esx_guntest:hasNotShotGun')
-AddEventHandler('esx_guntest:hasNotShotGun', function()
-	gunIsShot = false
-end)
-
-
+-- Koll om närmaste spelaren har krutstänk
 RegisterNetEvent('esx_guntest:checkGun')
 AddEventHandler('esx_guntest:checkGun', function(source)
-
 	local player, distance = ESX.Game.GetClosestPlayer()
 	if distance ~= -1 and distance <= 3.0 then	
-		
 		TaskStartScenarioInPlace(playerPed, "CODE_HUMAN_MEDIC_KNEEL", 0, true)
 		
 		Citizen.Wait(10000)
 		
 		ClearPedTasksImmediately(playerPed)
 
-		if (gunIsShot) then
-			ESX.ShowNotification('Personen har krutstänk på sina kläder')
-		else
-			ESX.ShowNotification('Inga spår av krut hittades')
-		end
-
-		gunIsShot = false	
-
+		-- Fråga servern om spelaren man undersöker (närmaste spelaren) har krutstänk på sig
+		ESX.TriggerServerCallback('esx_guntest:hasPlayerRecentlyFiredAGun', function(hasGunpowder)
+			if hasGunpowder then
+				ESX.ShowNotification('Personen har krutstänk på sina kläder')
+			else
+				ESX.ShowNotification('Inga spår av krut hittades')
+			end
+		end, GetPlayerServerId(player))
 	else
 		ESX.ShowNotification('Ingen person i närheten.')
 	end
 end)
 
+
+-- First load
+Citizen.CreateThread(function()
+	while ESX == nil do
+		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+		Citizen.Wait(0)
+	end
+
+	-- Hämta spelarens ped en gång
+	playerPed = GetPlayerPed(-1)
+end)
+
+-- Main thread
 Citizen.CreateThread(function()
 	while true do
 		Citizen.Wait(0)
 		
 		if IsPedShooting(GetPlayerPed(-1)) then
 			hasShot = true
+			-- Om man skjuter så lagra status på server 1 gång
+			TriggerServerEvent('esx_guntest:storePlayerGunpowderStatus')
 		end
 
-		if (hasShot) then
+		if hasShot then
+			-- Om man har skjutit så vänta en stund och ta sedan bort statusen
+			Citizen.Wait(GunpowderSaveTime)
+			hasShot = false
+			TriggerServerEvent('esx_guntest:removePlayerGunpowderStatus')
+		end
+
+		-- När man är nära en person så körs en serverfunktion 1 gång i framen, kommer ta extremt mycket på servern
+		--[[if (hasShot) then
 			local player, distance = ESX.Game.GetClosestPlayer()
 			if distance ~= -1 and distance <= 3.0 then
 				TriggerServerEvent('esx_guntest:storeStatusTrue', GetPlayerServerId(player))
 			end
 		else
 			TriggerServerEvent('esx_guntest:storeStatusFalse', GetPlayerServerId(player))
-		end
+		end]]
 	end
 end)
--- server.lua
+++ server.lua
@@ -1,13 +1,34 @@
 ESX               = nil
+local playersGunpowderStatuses = {}
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-RegisterServerEvent('esx_guntest:storeStatusTrue')
-AddEventHandler('esx_guntest:storeStatusTrue', function(source)
-	TriggerClientEvent('esx_guntest:hasShotGun', source)
+RegisterServerEvent('esx_guntest:storePlayerGunpowderStatus')
+AddEventHandler('esx_guntest:storePlayerGunpowderStatus', function()
+	local _source = source
+	-- spara source (serverId) i arrayen
+	table.insert(playersGunpowderStatuses, _source)
 end)
 
-RegisterServerEvent('esx_guntest:storeStatusFalse')
-AddEventHandler('esx_guntest:storeStatusFalse', function(source)
-	TriggerClientEvent('esx_guntest:hasNotShotGun', source)
+RegisterServerEvent('esx_guntest:removePlayerGunpowderStatus')
+AddEventHandler('esx_guntest:removePlayerGunpowderStatus', function()
+	-- tar bort spelaren från arrayen, reverse order for loop för att man inte ska ta bort ett element och sedan få nil
+	for i=#playersGunpowderStatuses, 1, -1 do
+		if playersGunpowderStatuses[i] == source then
+			table.remove(playersGunpowderStatuses, i)
+		end
+	end
+end)
+
+ESX.RegisterServerCallback('esx_guntest:hasPlayerRecentlyFiredAGun', function(source, cb, target)
+	local playerHasGunpowder = false
+
+	-- loopa igenom alla spelare som har krutstänk på sig (behöver inte va reverse order)
+	for i=#playersGunpowderStatuses, 1, -1 do
+		if playersGunpowderStatuses[i] == target then
+			playerHasGunpowder = true
+		end
+	end
+
+	cb(playerHasGunpowder)
 end)
